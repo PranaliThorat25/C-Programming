80fac20e8bb9913904c0dfee0103c212365f2b2c
Pranali Thorat <163281394+PranaliThorat25@users.noreply.github.com>
2024-10-09 22:31:30 +0530

    Add files via upload

---
+++ LIMITS.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<limits.h>
+main()
+{
+	printf("Signed short minimum value:%d\n",SHRT_MIN);
+	printf("Signed short maximum value:%d\n",SHRT_MAX);
+	printf("Unsigned short maximum value:%d\n",USHRT_MAX);
+	
+}
